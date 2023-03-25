eb4ad4bf4049b73c9f61da4aedd8552e48379cc1
Sercan Özen <srcn.ozn5@gmail.com>
2023-03-26 01:36:01 +0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::defaultView("vendor.pagination.yazilimegitimPagination");
 
         Carbon::setLocale(config("app.locale"));
+
+        URL::forceScheme('https');
     }
 }
