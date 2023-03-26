d80a1b8a4aa7fab5f13d6e270e5dcdd94aae2a6a
Sercan Özen <srcn.ozn5@gmail.com>
2023-03-26 03:45:03 +0300

    güncelleme 15

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-## Start PHP-FPM
-#/usr/sbin/php-fpm --nodaemonize --fpm-config /usr/local/etc/php-fpm.d/zzz_custom.conf &
-#
-## Start Nginx
-#/usr/sbin/nginx -g "daemon off;"
-
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 php-fpm -D
